2789b61444ab76a140e99855a6365752760e1e55
kikimyomyo <request1004@gmail.com>
2026-05-09 15:16:38 +0900

    deploy: joseph-paidion-8scenes

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "joseph-paidion-8scenes",
+  title: "요셉 8장면 — 파이디온판 (구약1 17과)",
+  sheet_tab: "joseph-paidion-8scenes",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
